85b542bae22157473ab116e4235248a30b3981af
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-01-16 09:20:25 +1300

    added a playground

--- appes6.js
+++ appes6.js
@@ -145,4 +145,28 @@ document.getElementById('book-form').addEventListener('submit',
       e.preventDefault();
     })
 
+    // Only change code above this line
+    function urlSlug(title) {
+      return title.toLowerCase().trim().split(/\s+/).join("-");
+    }
 
+    urlSlug(" Winter Is  Coming");
+
+    console.log(urlSlug(" Winter Is  Coming"));
+
+//process.env.MESSAGE_TYPE = "uppercase";
+
+const message="Hello json";
+
+app.use((req, res, next) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        req.message = req.message.toUpperCase();
+    }
+    next();
+    })
+
+
+    app.get("/json", (req, res) => {
+        res.json({"message": "Hello json"});
+        console.log(req.message)
+    })
